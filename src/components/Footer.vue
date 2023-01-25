b2674fcc235a17f1dcfa995fcad6d15e3c5ac971
Sabina Prodan <87321707+citrtoj@users.noreply.github.com>
2023-01-25 16:29:09 +0200

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h5>Made by <a href="https://citrtoj.github.io">citrtoj</a>, using Vue.js, the <a href="https://developer.spotify.com/">Spotify API</a> and the <a href="https://lyricsovh.docs.apiary.io/">lyrics.ovh</a> API.
-            <br>If it's inaccurate, blame the lyrics API, not me.<br>HMU on <a href="https://twitter.com/sircrossleg">Twitter</a> if this breaks.</h5>
+            <br>If it's inaccurate, blame the lyrics API, not me.</h5>
     </div>
 </template>
 
